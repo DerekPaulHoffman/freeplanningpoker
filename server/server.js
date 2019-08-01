7b893c9b7eb4f16f0ecdd6cc47988be08b890295
Grim_Leaper <derekpaulhoffman@gmail.com>
2019-07-31 20:25:47 -0400

    adding socket.id

--- server/server.js
+++ server/server.js
@@ -13,7 +13,8 @@ io.on('connection', (socket) => {
     socket.join(room);
     io.in(room).clients((error, clients) => {
       if (error) throw error;
-      console.log(clients); // => [6em3d4TJP8Et9EMNAAAA, G5p55dHhGgUnLUctAAAB]
+      console.log(clients);
+      socket.broadcast.in(room).emit('readRoomUsers', clients);
 });
     // var clients = io.sockets.clients(room);
     // socket.broadcast.in(room).emit('readRoomUsers',clients);
