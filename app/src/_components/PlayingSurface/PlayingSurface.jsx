1653d7202a8b6b974fd6e9e39a909a543117fe3b
Grim_Leaper <derekpaulhoffman@gmail.com>
2019-10-23 23:47:29 -0400

    Working flipping cards

--- app/src/_components/PlayingSurface/PlayingSurface.jsx
+++ app/src/_components/PlayingSurface/PlayingSurface.jsx
@@ -11,7 +11,9 @@ import "./PlayingSurface.scss";
 
 const PlayingSurface = (props) => {
   const [roomUsers, setRoomUsers] = useState([]);
-  const { socketSessionId, setSocketSessionId } = useSocketSessionId();
+  const {socketSessionId, setSocketSessionId } = useSocketSessionId();
+  const [sessionId, setSessionId] = useState();
+  const [showVotes, setShowVotes] = useState(false);
     
   const cardDimensions = { width: 2.25 * 12, height: 3.5 * 12 };
 
@@ -24,39 +26,59 @@ const PlayingSurface = (props) => {
 
 
   return (
-      <>
-        <ul className="playingSurface row">
-          {
-            roomUsers.map((roomUser, index) => {
-              console.log(roomUser.sessionId);
-              console.log(socketSessionId);
-                return (
-                  <li
-                    style={{
-                      height: `50vw`,
-                      order: `${
-                        roomUser.sessionId === socketSessionId
-                          ? "0"
-                          : index + roomUsers.length
-                      }`
-                    }}
-                    className="col-xs-4"
-                  >
-                    <Card
-                      key={index + 1}
-                      transform={0}
-                      width={`${cardDimensions.width}vw`}
-                      height={`${cardDimensions.height}vw`}
-                      className={`card surfaceCard`}
-                      cardNumber={roomUser.message}
-                      cardName={roomUser.userName}
-                    />
-                  </li>
-                );
-            })
-          }
-        </ul>
-      </>
+    <>
+      <button onClick={() => setShowVotes(true)}>Show Votes</button>
+      <ul className="playingSurface row">
+        {roomUsers.map((roomUser, index) => {
+          console.log(roomUser.sessionId);
+          console.log(sessionId);
+          return (
+            <li
+              style={{
+                height: `50vw`,
+                order: `${
+                  roomUser.sessionId === sessionId
+                    ? "0"
+                    : index + roomUsers.length
+                }`
+              }}
+              className="col-xs-4"
+            >
+              <div className="container">
+                <div className="deck">
+                  <div className={`cardasd ${showVotes != "" && "flipped"}`}>
+                    <div className="front face">
+                      <Card
+                        key={index + 1}
+                        transform={0}
+                        width={`${cardDimensions.width}vw`}
+                        height={`${cardDimensions.height}vw`}
+                        className={`card surfaceCard ${(roomUser.message) && 'showReady'}`}
+                        cardNumber={
+                          roomUser.sessionId === sessionId && roomUser.message
+                        }
+                        cardName={roomUser.userName}
+                      />
+                    </div>
+                    <div class="back face">
+                      <Card
+                        key={index + 1}
+                        transform={0}
+                        width={`${cardDimensions.width}vw`}
+                        height={`${cardDimensions.height}vw`}
+                        className={`card surfaceCard`}
+                        cardNumber={roomUser.message}
+                        cardName={roomUser.userName}
+                      />
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </li>
+          );
+        })}
+      </ul>
+    </>
   );
 };
 
